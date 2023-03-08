a87f45a3ca152e161275016c105c9a4aa853d777
iamgaddiel <ighotagaddiel98@gmail.com>
2023-03-08 11:21:46 -0500

    Added all models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import (
     User,
-    Retirement,
+    Retirement, 
+    GiftCardRequest,
+    Vacation,
+    News,
+    Gift,
+    Replacement,
+    Operations,
 )
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Retirement)
+admin.site.register(GiftCardRequest)
+admin.site.register(Vacation)
+admin.site.register(News)
+admin.site.register(Gift)
+admin.site.register(Replacement)
+admin.site.register(Operations)
